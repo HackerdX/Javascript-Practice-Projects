63ed9945a63b2ab8fd87dc34896cf7a248f5a7eb
HackerdX <cooleasycodebyanmol@gmail.com>
2024-02-24 22:01:20 +0530

    dark mode activated

--- Light-And-Dark-Mode/readme.md
+++ Light-And-Dark-Mode/readme.md
@@ -1,4 +1,7 @@
 # Resources:
 
-Dark theme docs: https://blog.prototypr.io/how-to-design-a-dark-theme-for-your-android-app-3daeb264637
-Undraw Illustrations: https://undraw.co/illustrations
+- Dark theme docs: https://blog.prototypr.io/how-to-design-a-dark-theme-for-your-android-app-3daeb264637
+- Undraw Illustrations: https://undraw.co/illustrations
+- Toggle Switch HTML/CSS: https://www.w3schools.com/howto/howto_css_switch.asp 
+- OnChange event for switch: https://www.w3schools.com/jsref/event_onchange.asp
+- 
---
+++ Light-And-Dark-Mode/script.js
@@ -0,0 +1,41 @@
+let toggleSwitch = document.querySelector('input[type="checkbox"]');
+let toggleIcon = document.querySelector('.mode');
+let headerBackground = document.querySelector('.header');
+let image1 = document.getElementById('image1');
+let image2 = document.getElementById('image2');
+let image3 = document.getElementById('image3');
+
+const darkMode = () => {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    // Header
+    toggleIcon.children[0].textContent = "Dark Mode";
+    toggleIcon.children[1].classList.add('fa-moon');
+    toggleIcon.children[1].classList.remove('fa-sun');
+    headerBackground.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    // Images
+    image1.src = "img/undraw_proud_coder_dark.svg";
+    image2.src = "img/undraw_feeling_proud_dark.svg";
+    image3.src = "img/undraw_conceptual_idea_dark.svg";
+}
+
+const lightMode = () => {
+    document.documentElement.setAttribute('data-theme', 'light');
+    // Header
+    toggleIcon.children[0].textContent = "Light Mode";
+    toggleIcon.children[1].classList.remove('fa-moon')
+    toggleIcon.children[1].classList.add('fa-sun');
+    headerBackground.style.backgroundColor = 'rgb(255 255 255 / 50%)'
+
+    // Images
+    image1.src = "img/undraw_proud_coder_light.svg";
+    image2.src = "img/undraw_feeling_proud_light.svg";
+    image3.src = "img/undraw_conceptual_idea_light.svg";
+}
+
+toggleSwitch.addEventListener('change', (event) => {
+    if(event.target.checked) {
+        darkMode();
+    } else {
+        lightMode();
+    }
+})
